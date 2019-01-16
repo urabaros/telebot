9f73e4bfad9f92a6087b4a4e6ef86dd32572ea3f
urabaros <alexeykirpa@ya.ru>
2019-01-16 15:26:08 +0300

    I just trying use heroku with python

---
+++ myfirstbot.py
@@ -0,0 +1,76 @@
+import requests
+import datetime
+ 
+token = "703529722:AAH5grPrk_qT0XhPx2IL_XqrpyaVwOLE6TE"
+class BotHandler: 
+
+	def __init__(self, token):
+		self.token = token
+		self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+	def get_updates_json(self, offset=None, timeot=30):
+		method = 'getUpdates'
+		params = {'timeot' : timeot, 'offset' : offset } 
+		resp = requests.get(self.api_url + method, params)
+		result_json = resp.json()['result']
+		return result_json
+
+	def send_message(self, chat_id, text):  
+		params = {'chat_id': chat_id, 'text': text}
+		method = 'sendMessage'
+		resp = requests.post(self,api_url + method, params)
+		return resp
+  
+	def get_last_update(self):
+		get_results = self.get_updates()
+
+		if len(get_results) > 0:
+			last_update = get_results[-1]
+		else:
+			last_update = get_results[len(get_results)] 
+		
+		return last_update
+
+	def get_chat_id(update):  
+		chat_id = update['message']['chat']['id']
+		return chat_id
+
+greet_bot = BotHandler(token)  
+greetings = ('hello', 'hi', 'greetings', 'sup')  
+now = datetime.datetime.now()
+ 
+ 
+def main():  
+	new_offset = None
+	today = now.day
+	hour = now.hour
+	 
+	while True:
+		greet_bot.get_updates(new_offset)
+
+		last_update = greet_bot.get_last_update()
+		 
+		last_update_id = last_update['update_id']
+		last_chat_text = last_update['message']['text']
+		last_chat_id = last_update['message']['chat']['id']
+		last_chat_name = last_update['message']['chat']['first_name']
+ 
+		if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+			greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+			today += 1
+ 
+		elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+			greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+			today += 1
+ 
+		elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+			greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+			today += 1
+ 
+		new_offset = last_update_id + 1
+ 
+if __name__ == '__main__':  
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit()
